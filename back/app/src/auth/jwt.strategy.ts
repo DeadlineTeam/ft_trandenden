79dca9d128b738fe48afd645e2198e9f2d5bb94b
ELORCHI <elorchie@gmail.com>
2022-12-21 21:51:46 +0100

    fixing jwt header

--- back/app/src/auth/jwt.strategy.ts
+++ back/app/src/auth/jwt.strategy.ts
@@ -2,11 +2,23 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
+
+const getAuthCookie = (req) => {
+	console.log(req.cookies, "dfdsfsdfsdfdsfds")
+	  if (req && req.cookies) {
+		if (req.cookies['Authorization'] != null)
+		{
+			return req.cookies['Authorization'].split(' ')[1];
+		}
+	  }
+	  return null;
+	}	
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: getAuthCookie,
       ignoreExpiration: true,
       secretOrKey: "HelloWorld",
     });
