7016f14570ceee80ce1e4272f6bbdba0351ea446
xatify <abbouzid@student.1337.ma>
2022-12-22 05:48:16 +0100

    adding cors to Gateway and credentials requirements verify user on connection

--- back/app/src/game/game.gateway.ts
+++ back/app/src/game/game.gateway.ts
@@ -1,29 +1,42 @@
-import { ConnectedSocket, WebSocketGateway } from "@nestjs/websockets";
+import { ConnectedSocket, WebSocketGateway, OnGatewayDisconnect, OnGatewayConnection } from "@nestjs/websockets";
 import { WebSocketServer, SubscribeMessage, MessageBody } from "@nestjs/websockets"
 import { GameService } from "./services/game.service";
 import { Server, Socket } from "socket.io";
+import { AuthService } from "src/auth/auth.service";
 
 @WebSocketGateway ({
-	cors: '*',
-	namespace: "/playgame"
+	cors: {
+		origin: 'http://localhost:3000',
+		credentials: true
+	},
+	namespace: "playgame"
 })
 
 
-export class GameGateway {
-	constructor (private readonly gameService: GameService) {}
+export class GameGateway implements OnGatewayDisconnect, OnGatewayConnection {
+	constructor (
+		private readonly gameService: GameService,
+		private readonly authService: AuthService) {}
 
 	@WebSocketServer ()
 	server: Server;
 
+	
+	handleConnection(@ConnectedSocket () client: Socket) {		
+		const payload = this.authService.verify(decodeURI (client.handshake?.headers?.cookie).replace ("Authorization=Bearer ", ""))
+		if (!payload) {
+			client.disconnect ();
+		}
+	}
+	
+	
+	handleDisconnect(@ConnectedSocket () client: Socket) {
+		this.gameService.leaveMatch (client)
+	}
+	
 	@SubscribeMessage ("play")
 	setToPlay (@ConnectedSocket () client: Socket, @MessageBody () data: string): void {
-		//console.log (data)
 		this.gameService.joinQueue(client, data);
-	}
-
-	@SubscribeMessage ("disconnect")
-	handleDisconnect (@ConnectedSocket () client: Socket, @MessageBody () data: string): void {
-		this.gameService.leaveMatch (client);
 	}
 
 	@SubscribeMessage ("input")
