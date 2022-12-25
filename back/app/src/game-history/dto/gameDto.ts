e1acd175d25ffa0815d415adf3904aedf8adace0
ELORCHI <elorchie@gmail.com>
2022-12-25 11:37:44 +0100

    game2.0

--- back/app/src/game-history/dto/gameDto.ts
+++ back/app/src/game-history/dto/gameDto.ts
@@ -2,7 +2,6 @@ import { gameMode} from "@prisma/client";
 import { gameStatus } from "@prisma/client";
 
 export class GameDto {
-	mode : gameMode;
 	status : gameStatus;
 	player1: string;
 	player2: string;
