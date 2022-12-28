6fc5264dcc5b7bc0b72aa01890525ac8a5133a7b
xatify <abbouzid@student.1337.ma>
2022-12-28 09:20:48 +0100

    hashing the password before storing in db, if room has protected visibility

--- back/app/src/room/room.service.ts
+++ back/app/src/room/room.service.ts
@@ -2,10 +2,25 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 import { VISIBILITY } from '@prisma/client';
 import { ROLE } from '@prisma/client';
 import { MemberService } from 'src/member/member.service';
+
+const visibitymap = (visibility: string) => {
+	if (visibility === 'Public') {
+		return VISIBILITY.PUBLIC;
+	} else if (visibility === 'Private') {
+		return VISIBILITY.PRIVATE;
+	} else {
+		return VISIBILITY.PROTECTED;
+	}
+}
+
+
+
+
 
 @Injectable()
 export class RoomService {
@@ -24,11 +39,17 @@ export class RoomService {
 			}
 		}
 
+		let pass = createRoomDto.password;
+		if (createRoomDto.visibility === 'Protected' && createRoomDto.password !== undefined) {
+			const passwd = await bcrypt.hash (createRoomDto.password, 10);
+			pass = passwd;
+		}
+
 		const room = await this.prisma.room.create({
 			data: {
 				name: createRoomDto.name,
-				visibility: createRoomDto.visibility === 'public' ? VISIBILITY.PUBLIC : createRoomDto.visibility === 'private' ? VISIBILITY.PRIVATE : VISIBILITY.PROTECTED,
-				passwd: createRoomDto.password,
+				visibility: visibitymap(createRoomDto.visibility),
+				passwd: pass,
 			}
 		});
 		await this.member.addMember(room.id, ownerId);
@@ -38,6 +59,6 @@ export class RoomService {
 				await this.member.addMember(room.id, user);
 			}
 		}
-		return room;
+		return {id: room.id, name: room.name, visibility: room.visibility};
 	}
 }
