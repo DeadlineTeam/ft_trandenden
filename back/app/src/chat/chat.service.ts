f63e57230ecda62215e09384e0295596ae3ef782
xatify <abbouzid@student.1337.ma>
2022-12-31 06:30:10 +0100

    service to handle room messages

--- back/app/src/chat/chat.service.ts
+++ back/app/src/chat/chat.service.ts
@@ -1,9 +1,38 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { MessageService } from 'src/message/message.service';
+import { FriendService } from 'src/friend/friend.service';
+import { UsersService } from 'src/users/users.service';
+import { MemberService } from 'src/member/member.service';
+import { ChatGateway } from './chat.gateway';
 
 @Injectable()
 export class ChatService {
 	constructor (
-		private readonly prisma: PrismaService,
-	) {}
+		@Inject (forwardRef (() => ChatGateway))
+		private readonly chatGateway: ChatGateway,
+		private readonly message: MessageService,
+		private readonly friend: FriendService,
+		private readonly member: MemberService,
+		) {}
+
+	async createMessage (senderId: number, message: {content: string, roomId: number}) {
+		if (!message.content || !message.roomId)
+			return;
+		// check if the user is a member of the room
+		// or if the user is banned or muted
+		// don't save create the message
+		const member = await this.member.getMember (message.roomId, senderId);
+		if (!member || member.banned || member.muted)
+			return;
+
+		const newMessage = await this.message.newMessage (senderId, message);
+		if (newMessage) {
+			const blockedFriends = await this.friend.BlockedFriends (senderId);
+			const broadcastTo = await this.member.getMembersIds (message.roomId);
+			const broadcastToUnblocked = broadcastTo.filter (id => !blockedFriends.includes (id));
+			const messageFormated = this.message.messageFormate (newMessage);
+			console.log (broadcastToUnblocked);
+			this.chatGateway.broadcastToRoom ("message", message.roomId, broadcastToUnblocked, messageFormated);
+		}
+	}
 }
