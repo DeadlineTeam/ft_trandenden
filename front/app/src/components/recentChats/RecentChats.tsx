37bad655c1c46eed419e65ddec04283f9200c31e
ytaousi <yassir.taous@gmail.com>
2023-01-01 21:04:58 +0100

    serious chat

--- front/app/src/components/recentChats/RecentChats.tsx
+++ front/app/src/components/recentChats/RecentChats.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
 import './RecentChats.css'
 
 const RecentChats = () => {
+    
     return (
-        <div className="recentConversations">
-            <img className="recentConversationsImg" src={require('../../SolidSnake.png')} alt="Snake"/>
-            <span className="recentConversationsName">Solid Snake</span>
+        <div className ="recentConversations" >
+            <img className="recentConversationsImg" src={require('../../LiquidSnake.png')} alt="Snake"/>
+            <span className="recentConversationsName">Liquid Snake</span>
         </div>
     )
 }
