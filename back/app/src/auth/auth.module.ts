e3d8b07909f90414d128a347b3b320a027c3794b
ELORCHI <eel-orch@student.1337.ma>
2023-01-02 22:52:50 +0100

    load jwt secret from env variables

--- back/app/src/auth/auth.module.ts
+++ back/app/src/auth/auth.module.ts
@@ -8,16 +8,25 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import {TwofaService} from './2fa.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [UsersModule, PassportModule.register({failureRedirect: 'localhost:3000'}), 
-	JwtModule.register({
-		secret: 'HelloWorld',
-		signOptions: { expiresIn: '7d'}
+	JwtModule.registerAsync({
+		imports: [ConfigModule],
+      	inject: [ConfigService],
+		useFactory: async (configService: ConfigService) => ({
+			secret: configService.get('JWT_SECRET'),
+			signOptions: {
+				expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}`,
+			},
+		}),
 	})],
-  providers: [AuthService, FortyTwoStrategy, JwtStrategy, TwofaService],
-  exports: [AuthService, TwofaService],
-  controllers: [AuthController],
+	providers: [AuthService, FortyTwoStrategy, JwtStrategy, TwofaService],
+	exports: [AuthService, TwofaService],
+	controllers: [AuthController],
 })
 export class AuthModule {}
+
+// secret: 'HelloWorld',
+// signOptions: { expiresIn: '7d'}
