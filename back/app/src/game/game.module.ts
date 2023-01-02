74d254875485cb3fb01ee196b906f5a0ffdfaaf8
xatify <abbouzid@student.1337.ma>
2023-01-02 22:45:38 +0100

    adding controller and using onlineService to send notifications

--- back/app/src/game/game.module.ts
+++ back/app/src/game/game.module.ts
@@ -1,17 +1,24 @@
-import { Module } from "@nestjs/common";
+import { Controller, Module } from "@nestjs/common";
 import { GameGateway } from "./game.gateway";
 import { GameService } from "./services/game.service";
 import { UsersModule } from "src/users/users.module";
 import { AuthModule } from "src/auth/auth.module";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { GameHistoryModule } from "src/game-history/game-history.module";
+import { GameController } from "./game.controller";
+import { OnlineModule } from "src/online/online.module";
+
 @Module ({
 	imports: [
 		UsersModule,
 		AuthModule,
 		PrismaModule,
 		GameHistoryModule,
+		OnlineModule,
 	],
-	providers: [GameService, GameGateway]
+	providers: [GameService, GameGateway],
+	controllers: [
+		GameController
+	]
 })
 export class GameModule {}
