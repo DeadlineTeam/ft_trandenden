8b12ec79973eedbce8a3810b9b22318cef06f11d
xatify <abbouzid@student.1337.ma>
2023-01-04 03:24:07 +0100

    logout the user everywhere

--- back/app/src/profile/profile.module.ts
+++ back/app/src/profile/profile.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
 import { GameHistoryModule } from 'src/game-history/game-history.module';
 import { FriendModule } from 'src/friend/friend.module';
+import { OnlineModule } from 'src/online/online.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, GameHistoryModule, FriendModule],
+  imports: [PrismaModule, UsersModule, GameHistoryModule, FriendModule, OnlineModule],
   providers: [ProfileService],
   controllers: [ProfileController],
   exports: [ProfileService],
