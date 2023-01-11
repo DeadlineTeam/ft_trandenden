fe8c2636d7b0802fcc62ab98ef4ac98af5c550e7
xatify <abbouzid@student.1337.ma>
2023-01-11 02:31:56 +0100

    room creation error handling

--- back/app/src/room/dto/create-room.dto.ts
+++ back/app/src/room/dto/create-room.dto.ts
@@ -1,21 +1,23 @@
 import {IsArray, IsString, ValidateIf,IsNotEmpty, IsOptional, ValidationOptions } from 'class-validator';
 export class CreateRoomDto {
-	@IsString()
-	@IsNotEmpty()
+	// @IsString()
+	// @IsNotEmpty()
 	name: string;
 
-	@IsString()
-	@IsNotEmpty()
+	// @IsString()
+	// @IsNotEmpty()
   	visibility: string;
+	
+	
 	@IsString()
 
-	@ValidateIf((object: any) => object.visibility !== undefined && object.visibility === 'Protected')
-	@IsNotEmpty({
-		message: 'Password is required for protected rooms',
-		when: (object: any) => object.password !== undefined
-	  } as ValidationOptions)
+	// @ValidateIf((object: any) => object.visibility !== undefined && object.visibility === 'Protected')
+	// @IsNotEmpty({
+	// 	message: 'Password is required for protected rooms',
+	// 	when: (object: any) => object.password !== undefined
+	//   } as ValidationOptions)
 	password: string;
 
-	@IsArray()
+	// @IsArray()
 	users: number [];
 }
